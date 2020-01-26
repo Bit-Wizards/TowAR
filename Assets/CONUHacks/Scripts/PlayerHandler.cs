0f69ad774ae461d81c009ab3ef834221d74ac2ea
Grovesy <99tgroves@gmail.com>
2020-01-25 22:37:36 -0500

    Adding Button Functions

--- Assets/CONUHacks/Scripts/PlayerHandler.cs
+++ Assets/CONUHacks/Scripts/PlayerHandler.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHandler : MonoBehaviour
 {
+    public Button soilderBlueBtn; 
+
+    private enum SoilderSelectType
+    {
+        SOLDIER_BLUE,
+        SOLDIER_RED
+    };
+
+    private int currentSelectedSoilder;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSelectedSoilder = (int) SoilderSelectType.SOLDIER_BLUE;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SoilderBlueBtnClick()
+    {
+        currentSelectedSoilder = (int)SoilderSelectType.SOLDIER_BLUE;
+    }
+
+    public void SoilderRedBtnClick()
+    {
+        currentSelectedSoilder = (int)SoilderSelectType.SOLDIER_RED;
     }
 }
